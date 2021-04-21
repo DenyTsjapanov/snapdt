0c1bfa7f7b4092da557f009bfa063ec0d77c291b
DenyTsjapanov <d.tsjapanov@gmail.com>
2021-04-21 21:28:03 +0000

    Added namescapes to apps

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'portfolio'
+
 urlpatterns = [
-    path('', views.all_items, name='items')
+    path('', views.all_items, name='items'),
+    path('<item_id>', views.item_detail, name='item_detail'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 # Create your views here.
@@ -13,3 +13,14 @@ def all_items(request):
     }
 
     return render(request, 'portfolio/portfolio.html', context)
+
+
+def item_detail(request, item_id):
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'portfolio/item_detail.html', context)
