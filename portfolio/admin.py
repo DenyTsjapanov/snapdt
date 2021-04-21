09bb7b7411d9481e5c1cb661438fc3702540feba
DenyTsjapanov <d.tsjapanov@gmail.com>
2021-04-21 11:38:30 +0000

    Customized admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Item, Category
 
 # Register your models here.
-admin.site.register(Item)
-admin.site.register(Category)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'rating',
+        'image',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
