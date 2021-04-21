1afa7fb59320588cba3cf02e8056ba9dcb733bfe
DenyTsjapanov <d.tsjapanov@gmail.com>
2021-04-21 12:19:40 +0000

    Added portfolio views and template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+
+def all_items(request):
+
+    items = Item.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'portfolio/portfolio.html', context)
