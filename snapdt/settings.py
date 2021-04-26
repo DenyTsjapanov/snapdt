7976e4ee663db3407f3838979307f3401a9dcfd2
DenyTsjapanov <d.tsjapanov@gmail.com>
2021-04-26 23:14:13 +0000

    Added bag context and delivery logic

--- snapdt/settings.py
+++ snapdt/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.context.bag_contents',
             ],
         },
     },
@@ -153,6 +154,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THERSHOLD = 50
+STANDARD_DELIVERY_RATE = 6,75
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
