c72b5b2e6fc874ac42064d8fd940e16df84769d3
DenyTsjapanov <d.tsjapanov@gmail.com>
2021-04-27 12:54:48 +0000

    Added toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from portfolio.models import Item
 
 # Create your views here.
 
@@ -9,6 +12,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
 
+    item = Item.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -17,6 +21,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {item.name} to you bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
