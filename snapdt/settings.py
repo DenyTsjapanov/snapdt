e2c76e834bea633b9a5b3347e3a15cc8ea0c50a1
DenyTsjapanov <d.tsjapanov@gmail.com>
2021-04-28 23:01:01 +0000

    Added basic checkout functionality

--- snapdt/settings.py
+++ snapdt/settings.py
@@ -171,8 +171,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_RATE = 6.75
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
