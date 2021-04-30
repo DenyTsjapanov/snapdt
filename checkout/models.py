c4d3224f83c97f87ea5b6899a0ebcb010049e270
DenyTsjapanov <d.tsjapanov@gmail.com>
2021-04-30 17:06:23 +0000

    Added profiles app

--- checkout/models.py
+++ checkout/models.py
@@ -7,10 +7,13 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from portfolio.models import Item
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -28,7 +31,8 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
 
@@ -71,7 +75,7 @@ class OrderLineItem(models.Model):
         max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
-        
+
         # Override the original save method to set the lineitem total
         # and update the order total.
 
--- snapdt/settings.py
+++ snapdt/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'portfolio',
     'bag',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
