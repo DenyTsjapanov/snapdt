6fe080323e91c80986548243ffc8c44be87d6e80
DenyTsjapanov <d.tsjapanov@gmail.com>
2021-05-01 01:45:15 +0000

    Added cache control

--- snapdt/settings.py
+++ snapdt/settings.py
@@ -181,6 +181,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+     # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 CET',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'snapdt'
     AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
