f36020419173e7d86255a3ef8416ffd13cbe0193
DenyTsjapanov <d.tsjapanov@gmail.com>
2021-05-01 00:40:34 +0000

    Attempt migration and deployment

--- snapdt/settings.py
+++ snapdt/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-s2r4ijsuvdg1!=8z=eoamt2oa4ts!+ycm*9_x0=0vvrt0i^zhw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['snapdt.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,12 +124,17 @@ WSGI_APPLICATION = 'snapdt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
